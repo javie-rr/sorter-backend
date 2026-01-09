09ae91e1c9b59167fd5b9236bebb7fbe06f87638
Javier <javier4g.dam@gmail.com>
2026-01-09 17:25:12 -0600

    Configurar panel admin de la app user

--- users/admin.py
+++ users/admin.py
@@ -6,6 +6,19 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    #pass
     model = User
+
     list_display = ['id','first_name','last_name','second_last_name','email','password']
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Personal info', {
+            'fields': ('second_last_name',),
+        }),
+    )
+
+    # To include the new field in the add user form as well
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Personal info', {
+            'fields': ('first_name','last_name','second_last_name',),
+        }),
+    )
