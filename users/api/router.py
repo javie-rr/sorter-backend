8e31fe9c99992fbc92da4f734b99131188d900ab
Javier <javier4g.dam@gmail.com>
2026-01-09 17:30:19 -0600

    Personalizar vista TokenObtainPairView

--- users/api/router.py
+++ users/api/router.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from .views import CustomTokenObtainPairView
 
 urlpatterns = [
-    path('auth/login', TokenObtainPairView.as_view(), name='login'),
+    path('auth/login', CustomTokenObtainPairView.as_view(), name='login'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
 ]
---
+++ users/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        user = self.user
+
+        data['user'] = {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'second_last_name': user.second_last_name,
+        }
+
+        return data
