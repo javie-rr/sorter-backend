4b130dfff7974c6e6b76af2cdb49786c3241a02d
Javier <javier4g.dam@gmail.com>
2026-01-20 14:33:37 -0600

    feat: cambiar formato de salida json organization_types app

--- organization_types/api/views.py
+++ organization_types/api/views.py
@@ -1,7 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from organization_types.models import OrganizationType
 from organization_types.api.serializers import OrganizationTypeSerializer
+from rest_framework.response import Response
 
 class OrganizationTypeViewSet(ModelViewSet):
     queryset = OrganizationType.objects.all()
     serializer_class = OrganizationTypeSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response({
+            "organization_types": serializer.data
+        })
