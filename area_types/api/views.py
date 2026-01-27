cb82f22b75282427fe4c6ba6312e554f2997ad31
Javier <javier4g.dam@gmail.com>
2026-01-27 10:31:08 -0600

    feat: usar BaseViewset en la vista area_types

--- area_types/api/views.py
+++ area_types/api/views.py
@@ -1,16 +1,8 @@
-from rest_framework.viewsets import ModelViewSet
+from core.views import BaseViewSet
 from area_types.models import AreaType
 from area_types.api.serializers import AreaTypeSerializer
-from rest_framework.response import Response
 
-class AreaTypeViewSet(ModelViewSet):
+class AreaTypeViewSet(BaseViewSet):
     queryset = AreaType.objects.all()
     serializer_class = AreaTypeSerializer
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-
-        return Response({
-            "area_types": serializer.data
-        })
+    response_key = "area_types"
