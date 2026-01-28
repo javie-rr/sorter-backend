78f27b580e83d2d983d5d3d6713db7b878b0c05c
Javier <javier4g.dam@gmail.com>
2026-01-27 18:17:36 -0600

    fix: registrar rutas de la app usuario

--- users/api/router.py
+++ users/api/router.py
@@ -1,16 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenVerifyView
 from users.api.views import CustomTokenObtainPairView, CustomTokenRefreshView, LogoutView
 from rest_framework.routers import DefaultRouter 
 from users.api.views import UserViewSet
+
+router_users = DefaultRouter(trailing_slash=False) 
+router_users.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('auth/login', CustomTokenObtainPairView.as_view(), name='login'),
     path('token/refresh', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
     path('auth/logout', LogoutView.as_view(), name='logout'),
+    path('', include(router_users.urls)),
 ]
-
-router_users = DefaultRouter(trailing_slash=False) 
-router_users.register(r'users', UserViewSet, basename='users')
-urlpatterns = router_users.urls
